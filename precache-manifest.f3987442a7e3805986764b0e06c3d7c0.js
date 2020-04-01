7919dd62b273d4a29c98909c3588dddfe7ffd125
calumbutler <calum@gigantum.com>
2020-03-31 23:21:58 -0400

    Updates

---
+++ precache-manifest.f3987442a7e3805986764b0e06c3d7c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb8322e7bd366a9461aaa8f35165fcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "640e0cc854d7d0fddfc4",
+    "url": "/static/css/main.f521d73f.chunk.css"
+  },
+  {
+    "revision": "bc2f7b51bf1dbfc36166",
+    "url": "/static/js/2.3e40d24a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e40d24a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640e0cc854d7d0fddfc4",
+    "url": "/static/js/main.e534a273.chunk.js"
+  },
+  {
+    "revision": "eb101a31fab05643d1e2",
+    "url": "/static/js/runtime-main.531caabc.js"
+  },
+  {
+    "revision": "df84f33ade5cbd9863290982be730c7c",
+    "url": "/static/media/ding.df84f33a.mp3"
+  }
+]);
